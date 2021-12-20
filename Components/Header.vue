636b38c3426eb9a2ca79bb833b7bcc17d05fe778
Jerome <jerome0caruso@gmail.com>
2021-12-20 14:54:38 -0600

    start of task tracker

---
+++ Components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+    <Header>
+        <h1>{{title}}</h1>
+        <Button text="Add Task" color="green"/>
+    </Header>
+</template>
+
+<script>
+import Button from './Button'
+
+export default {
+    name: 'Header',
+    props: {
+        title: String,
+    }, 
+    comments: {
+        Button
+    }
+}
+</script>
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
---
+++ Components/Tasks.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+    <div :key="task.id" v-for="task in tasks" >
+        <Task :task="task" />
+    </div>
+</div>
+</template>
+
+<script>
+import Tast from './Task'
+export default {
+    name: 'Tasks',
+    props: {
+        tasks: Array
+    },
+    components: {
+        Task,
+    }
+}
+</script>
